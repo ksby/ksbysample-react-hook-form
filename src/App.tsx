be20d58c013a8a6ee4fe21176ac8ef940abceb2d
ksby <kusa@db3.so-net.ne.jp>
2021-05-01 11:38:50 +0000

    yarn lint を実行しエラー・警告が出た箇所を修正しました

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-function App() {
-  return (
-    <div className="pt-4 pl-4 text-red-600 text-4xl font-extrabold">
-      React+Tailwind CSS
-    </div>
-  );
-}
+import {FC} from "react";
+
+const App: FC = () => (
+  <div className="pt-4 pl-4 text-red-600 text-4xl font-extrabold">
+    React+Tailwind CSS
+  </div>
+);
 
 export default App;
